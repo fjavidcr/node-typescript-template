b04b9a95667e38766445a7fd11a29fbb1399ec3e
Javi del Castillo <15940847+fjavidcr@users.noreply.github.com>
2023-11-11 21:50:35 +0100

    add User interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import type User from './interfaces/User'
+
 console.log("Hello, world!");
+
+let employee: User = {
+  id: '123',
+  firstName: "Emil",
+  lastName: "Andersson",
+  name(): string {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+console.log(employee.name(), employee)
